33955c720a8aec3de1d95d23de24f41db41606e0
Emanuele Favero <emafaux89@gmail.com>
2026-08-08 02:14:07 +0200

    refactor: nest providers with nest provider component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import {
   ProductsFiltersProvider,
 } from './features/products/context';
 
+const providers = [ProductsFiltersProvider, ProductsProvider];
+
+const Nest = ({ providers, children }) =>
+  providers.reduceRight(
+    (nested, Provider) => <Provider>{nested}</Provider>,
+    children,
+  );
+
 export const App = () => (
-  <ProductsFiltersProvider>
-    <ProductsProvider>
-      <RouterProvider router={router} />
-    </ProductsProvider>
-  </ProductsFiltersProvider>
+  <Nest providers={providers}>
+    <RouterProvider router={router} />
+  </Nest>
 );
